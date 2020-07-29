075761ade887a36e52966cc9298dca7140829e53
cscully93 <ciascu93@gmail.com>
2020-07-29 11:24:42 +0100

    adding property call to deletePet.java

--- src/main/java/apis/deletePet.java
+++ src/main/java/apis/deletePet.java
@@ -1,9 +1,11 @@
 package apis;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
+@PropertySource("application.properties")
 public class deletePet {
 
     @Value("${pet.id}")
