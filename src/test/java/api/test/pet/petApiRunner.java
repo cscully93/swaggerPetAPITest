4ccb53936920613dc39467f52479a9e007085a59
cscully93 <ciascu93@gmail.com>
2020-09-26 17:41:52 +0100

    added testng to project

--- src/test/java/api/test/pet/petApiRunner.java
+++ src/test/java/api/test/pet/petApiRunner.java
@@ -7,6 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(features = "classpath:features/",
         glue = "stepDefinitions",
         tags = "@petAPITest",
+        publish = true,
         plugin  = {"pretty",
                 "html:target/cucumber-pretty",
                 "json:target/cucumber.json"})
